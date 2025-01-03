da823086cf1f49e594c40070167f46e1ab87404e
Tayra <sotomezatayra@gmail.com>
2025-01-02 21:15:03 -0500

    Cors authorization

--- django_crud_api/settings.py
+++ django_crud_api/settings.py
@@ -125,3 +125,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# cors authorization
+CORS_ALLOWED_ORIGINS = []
