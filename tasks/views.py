6b448b03f17b6306859f89a137c5524b77734e04
Tayra <sotomezatayra@gmail.com>
2025-01-02 21:34:07 -0500

    queryset

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets # type: ignore
+from .serializer import TaskSerializer
+from .models import Task
 
 # Create your views here.
+class TaskView(viewsets.ModelViewSet):
+    serializer_class = TaskSerializer
+    queryser = Task.object.all()
